e217504791cc4ea3f36a8c2e2819d8a86dd2f1d8
ridoy206 <ridoymahmud678@gmail.com>
2021-10-05 17:29:01 +0600

    updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,19 +2,29 @@ import React from 'react';
 import "./Header.css";
 import { NavLink } from 'react-router-dom';
 import pic from '../../images/Header-Man-on-Laptop.jpg';
+import pic2 from "../../images/LogoMakerCa-1633404386659.png"
 
 const Header = () => {
     return (
         <div>
-            <nav className="nav-bar p-4 position-fixed w-100">
-                <NavLink className="mx-3 text-decoration-none fw-bold text-white" to="/home">Home</NavLink>
-                <NavLink className="mx-3 text-decoration-none fw-bold text-white" to="/about">About</NavLink>
-                <NavLink className="mx-3 text-decoration-none fw-bold text-white" to="/allservices">Services</NavLink>
-                <NavLink className="mx-3 text-decoration-none fw-bold text-white" to="/contact">Contact</NavLink>
-                <NavLink className="mx-3 text-decoration-none fw-bold text-white" to="/pricing">Pricing</NavLink>
+            <nav className="nav-bar  position-fixed w-100 row">
+                <div className="col-3">
+                    <img className="logo w-25" src={pic2} alt="" />
+                </div>
+                <div className="col-5 my-auto">
+                    <NavLink className="mx-3 text-decoration-none fw-bold text-white" to="/home">Home</NavLink>
+                    <NavLink className="mx-3 text-decoration-none fw-bold text-white" to="/about">About</NavLink>
+                    <NavLink className="mx-3 text-decoration-none fw-bold text-white" to="/allservices">Services</NavLink>
+                    <NavLink className="mx-3 text-decoration-none fw-bold text-white" to="/contact">Contact</NavLink>
+                    <NavLink className="mx-3 text-decoration-none fw-bold text-white" to="/pricing">Pricing</NavLink>
+                </div>
+                <div className="col-4 my-auto">
+                    <input className="p-1 me-2" type="text" name="" id="" />
+                    <button className="btn  btn-outline-success fw-bold mb-1 py-2 login">Log in</button>
+                </div>
             </nav>
             <div>
-                <img className="w-100 h-50" src={pic} alt="" />
+                <img className="w-100 h-25 positon-fixed" src={pic} alt="" />
             </div>
         </div>
     );
